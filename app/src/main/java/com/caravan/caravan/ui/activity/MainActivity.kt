68e5c8981ad12c2d234c329cbdd2adda7202186c
MrMansur <mirzayevmn96@gmail.com>
2022-05-22 03:04:45 -0700

    Dialoglar va itemlar qo'shildi.

--- app/src/main/java/com/caravan/caravan/ui/activity/MainActivity.kt
+++ app/src/main/java/com/caravan/caravan/ui/activity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.caravan.caravan.ui.activity
 
 import android.os.Bundle
 import com.caravan.caravan.R
+import com.caravan.caravan.utils.Dialog
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/caravan/caravan/utils/Dialog.kt
@@ -0,0 +1,77 @@
+package com.caravan.caravan.utils
+
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.caravan.caravan.R
+import com.caravan.caravan.databinding.AlertDialogBinding
+import com.caravan.caravan.databinding.DialogMessageBinding
+import com.caravan.caravan.databinding.DialogWarningBinding
+
+object Dialog {
+
+    fun showAlertDialog(context: Context, title: String, cancel: CancelInterface, ok: OkInterface) {
+        val dialog = Dialog(context)
+        val alertDialogBinding = AlertDialogBinding.inflate(LayoutInflater.from(context))
+        dialog.setContentView(alertDialogBinding.root)
+
+        alertDialogBinding.tvTitle.text = title
+        alertDialogBinding.btnYes.setOnClickListener{
+            ok.onClick()
+            dialog.dismiss()
+        }
+
+        alertDialogBinding.btnNo.setOnClickListener{
+            cancel.onClick()
+            dialog.dismiss()
+        }
+        dialog.window?.setBackgroundDrawable(context.getDrawable(R.drawable.ic_launcher_foreground))
+        dialog.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+    }
+
+    fun showDialogMessage(context: Context, title: String, photo: String, description: String, ok: OkInterface) {
+        val dialog = Dialog(context)
+        val msgDialogBinding = DialogMessageBinding.inflate(LayoutInflater.from(context))
+        dialog.setContentView(msgDialogBinding.root)
+
+        msgDialogBinding.tvTitle.text = title
+        Glide.with(context).load(photo).into(msgDialogBinding.imageView)
+        msgDialogBinding.tvMessage.text = description
+        msgDialogBinding.btnOk.setOnClickListener{
+            ok.onClick()
+            dialog.dismiss()
+        }
+
+        dialog.window?.setBackgroundDrawable(context.getDrawable(R.drawable.ic_launcher_foreground))
+        dialog.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+    }
+
+    fun showDialogWarning(context: Context, title: String, description: String, ok: OkInterface) {
+        val dialog = Dialog(context)
+        val warningBinding = DialogWarningBinding.inflate(LayoutInflater.from(context))
+        dialog.setContentView(warningBinding.root)
+
+        warningBinding.tvTitle.text = title
+        warningBinding.tvMessage.text = description
+
+        warningBinding.btnOk.setOnClickListener{
+            ok.onClick()
+            dialog.dismiss()
+        }
+        dialog.window?.setBackgroundDrawable(context.getDrawable(R.drawable.ic_launcher_foreground))
+        dialog.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+    }
+}
+
+interface CancelInterface {
+    fun onClick()
+}
+
+interface OkInterface {
+    fun onClick()
+}
