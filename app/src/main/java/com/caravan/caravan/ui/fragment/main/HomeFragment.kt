c8be44b73278e00425265bde68eb8fb279997918
OgabekDev <ogabekdev@gmail.com>
2022-05-22 19:20:54 +0500

    Intro Page is responsible now StatusBar color set

--- app/src/main/java/com/caravan/caravan/ui/fragment/main/HomeFragment.kt
+++ app/src/main/java/com/caravan/caravan/ui/fragment/main/HomeFragment.kt
@@ -3,11 +3,13 @@ package com.caravan.caravan.ui.fragment.main
 import android.os.Bundle
 import android.view.View
 import com.caravan.caravan.R
+import com.caravan.caravan.databinding.FragmentHomeBinding
 import com.caravan.caravan.ui.fragment.BaseFragment
+import com.caravan.caravan.utils.viewBinding
 
 class HomeFragment : BaseFragment(R.layout.fragment_home) {
 
-
+    private val binding by viewBinding { FragmentHomeBinding.bind(it) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
