50d68c5c255ada03fe9a636d4fdcea361fb7e1fa
matyaqubovdev <uzroot.pdp@gmail.com>
2022-06-04 19:12:40 +0500

    Turist to Guide

--- app/src/main/java/com/caravan/caravan/ui/fragment/guideOption/TuristGuideOptionFragment.kt
+++ app/src/main/java/com/caravan/caravan/ui/fragment/guideOption/TuristGuideOptionFragment.kt
@@ -7,16 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.caravan.caravan.R
 import com.caravan.caravan.databinding.FragmentTuristGuideOptionBinding
+import com.caravan.caravan.manager.SharedPref
+import com.caravan.caravan.model.Profile
+import com.caravan.caravan.network.ApiService
+import com.caravan.caravan.network.RetrofitHttp
 import com.caravan.caravan.ui.fragment.BaseFragment
+import com.caravan.caravan.utils.Dialog
+import com.caravan.caravan.utils.OkInterface
+import com.caravan.caravan.utils.UiStateObject
 import com.caravan.caravan.utils.viewBinding
+import com.caravan.caravan.viewmodel.guideOption.turistGuide.TuristGuideRepository
+import com.caravan.caravan.viewmodel.guideOption.turistGuide.TuristGuideViewModel
+import com.caravan.caravan.viewmodel.guideOption.turistGuide.TuristguideViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,6 +32,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TuristGuideOptionFragment : BaseFragment() {
+
+    private lateinit var profile: Profile
+    private var profileId: String? = null
+    private lateinit var viewModel: TuristGuideViewModel
     private val binding by viewBinding { FragmentTuristGuideOptionBinding.bind(it) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,12 +47,49 @@ class TuristGuideOptionFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupViewModel()
+        setupObservers()
         initViews()
     }
 
+    private fun setupObservers() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.profile.collect {
+                when (it) {
+                    is UiStateObject.LOADING -> {
+                        showLoading()
+                    }
+                    is UiStateObject.SUCCESS -> {
+                        dismissLoading()
+                        profile = it.data
+                    }
+                    is UiStateObject.ERROR -> {
+                        dismissLoading()
+                        Dialog.showDialogWarning(
+                            requireContext(),
+                            getString(R.string.str_no_connection),
+                            getString(R.string.str_try_again),
+                            object : OkInterface {
+                                override fun onClick() {
+
+                                }
+
+                            }
+                        )
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+
     private fun initViews() {
+        profileId = SharedPref(requireContext()).getString("profileId")
+        getProfileDetails()
         binding.apply {
             llRegisterGuide.setOnClickListener {
+                // if (profile.photo.isNullOrBlank())
                 findNavController().navigate(R.id.action_turistGuideOptionFragment_to_upgradeGuide1Fragment)
             }
             llGetInfo.setOnClickListener {
@@ -48,6 +97,31 @@ class TuristGuideOptionFragment : BaseFragment() {
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(intent)
             }
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(
+            this,
+            TuristguideViewModelFactory(TuristGuideRepository(RetrofitHttp.createService(ApiService::class.java)))
+        )[TuristGuideViewModel::class.java]
+    }
+
+    private fun getProfileDetails() {
+        if (profileId != null) {
+            viewModel.getProfile(profileId!!)
+        } else {
+            Dialog.showDialogWarning(
+                requireContext(),
+                getString(R.string.error),
+                getString(R.string.went_wrong),
+                object : OkInterface {
+                    override fun onClick() {
+
+                    }
+
+                }
+            )
         }
     }
 
---
+++ app/src/main/java/com/caravan/caravan/viewmodel/guideOption/turistGuide/TuristGuideViewModel.kt
@@ -0,0 +1,34 @@
+package com.caravan.caravan.viewmodel.guideOption.turistGuide
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.caravan.caravan.model.Profile
+import com.caravan.caravan.utils.UiStateObject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class TuristGuideViewModel(private val repository: TuristGuideRepository) : ViewModel() {
+
+    private val _profile = MutableStateFlow<UiStateObject<Profile>>(UiStateObject.EMPTY)
+    val profile = _profile
+
+    private val _updatedProfile = MutableStateFlow<UiStateObject<Profile>>(UiStateObject.EMPTY)
+    val updatedProfile = _updatedProfile
+
+    fun getProfile(id: String) = viewModelScope.launch {
+        _profile.value = UiStateObject.LOADING
+        try {
+            val profile = repository.getProfile(id)
+            if (!profile.isSuccessful) {
+                _profile.value = UiStateObject.ERROR(profile.message())
+            }
+            _profile.value = UiStateObject.SUCCESS(profile.body()!!)
+
+        } catch (e: Exception) {
+            _profile.value = UiStateObject.ERROR(e.localizedMessage ?: "No Connection")
+        }
+    }
+
+
+
+}
